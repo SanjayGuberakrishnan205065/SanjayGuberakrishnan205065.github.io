142ca4a61b8fbb548e71ff1294ae81062c31b342
Pragadesh BS <pragadeshbs@protonmail.com>
2024-02-19 08:25:42 +0530

    Update sponsor text color

--- src/components/home/Sponsors.jsx
+++ src/components/home/Sponsors.jsx
@@ -10,7 +10,7 @@ const Sponsors = () => {
   return (
     <section className="py-8 px-8 lg:py-20 bg-gray-200">
       <div className="container mx-auto text-center">
-        <div className="text-xl text-blue-950 mb-8">SPONSORED BY</div>
+        <div className="text-xl text-blue-gray-900 mb-8">SPONSORED BY</div>
         <div className="flex flex-wrap items-center justify-center gap-6">
           {sponsors.map((logo, key) => (
             <img
