c5976edc48eeae26e25110c5c652af061f0dce4a
Pragadesh BS <pragadeshbs@protonmail.com>
2024-02-19 22:11:24 +0530

    link vercel backend

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // apiUrl: "https://mutex-api.azurewebsites.net",
-  apiUrl: "http://192.168.0.100:5000",
+  apiUrl: "https://samhita24-backend.vercel.app",
+  // apiUrl: "http://192.168.0.100:5000",
 };
 
 export default config;
