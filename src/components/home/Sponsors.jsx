52c6d30beb7c77a5cc3572c94b2c07661b642062
Pragadesh BS <pragadeshbs@protonmail.com>
2024-02-22 10:50:12 +0530

    Add new sponsor logos and update sponsor section

--- src/components/home/Sponsors.jsx
+++ src/components/home/Sponsors.jsx
@@ -1,24 +1,17 @@
 const Sponsors = () => {
   const sponsors = [
-    "coinbase",
-    "spotify",
-    "pinterest",
-    "google",
-    "amazon",
-    "netflix",
+    "guvi.png",
+    "kalimark.webp",
+    "finolex.png",
+    "shortflix.webp",
   ];
   return (
-    <section className="py-8 px-8 lg:py-20 bg-gray-200">
+    <section className="py-8 px-8 lg:py-20 bg-white">
       <div className="container mx-auto text-center">
         <div className="text-xl text-blue-gray-900 mb-8">SPONSORED BY</div>
-        <div className="flex flex-wrap items-center justify-center gap-6">
+        <div className="flex flex-wrap items-center justify-evenly gap-6">
           {sponsors.map((logo, key) => (
-            <img
-              key={key}
-              src={`/logos/logo-${logo}.svg`}
-              alt={logo}
-              className="w-40"
-            />
+            <img key={key} src={`/logos/${logo}`} alt={logo} className="w-40" />
           ))}
         </div>
       </div>
