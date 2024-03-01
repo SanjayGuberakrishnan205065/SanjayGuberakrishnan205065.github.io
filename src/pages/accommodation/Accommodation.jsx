7e644a85140fd8670d28eacab7117f6d5fd264ad
Pragadesh BS <pragadeshbs@protonmail.com>
2024-03-01 12:28:10 +0530

    Update accommodation details and options

--- src/pages/accommodation/Accommodation.jsx
+++ src/pages/accommodation/Accommodation.jsx
@@ -8,11 +8,11 @@ const Accommodation = () => {
       <Typography variant="h2">Accommodation</Typography>
       <div className="my-3 text-xl">
         <div className="my-3">
-          Accommodation is available to the participants in MIT Hostel for a fee
-          of ₹400 for 24 hours.
+          Accommodation is available to the participants in MIT Hostel from a
+          starting price of ₹250 for 24 hours.
         </div>
         <div className="my-3">
-          The accomodation fee includes breakfast, lunch and dinner.
+          Food charge includes breakfast, lunch and dinner.
         </div>
         <div className="my-3">Accommodation available within MIT Premises.</div>
       </div>
@@ -20,16 +20,30 @@ const Accommodation = () => {
         <Typography variant="h3">Accommodation Tickets</Typography>
         <div className="flex flex-col md:flex-row gap-5 justify-evenly">
           <AccommodationCard
-            title={"Accommodation"}
-            price={400}
-            checkoutId="accomodation_24hrs"
-            description="Upto 24 hrs Stay"
+            title={"Accommodation 24hrs (No Food)"}
+            price={250}
+            checkoutId="accommodation_24hrs_no_food"
+            description="Upto 24 hrs Stay. Food NOT included"
           />
           <AccommodationCard
-            title={"Accommodation"}
+            title={"Accommodation 48hrs (No Food)"}
+            price={500}
+            checkoutId="accommodation_48hrs_no_food"
+            description="Upto 48 hrs Stay. Food NOT included"
+          />
+        </div>
+        <div className="flex flex-col md:flex-row gap-5 justify-evenly">
+          <AccommodationCard
+            title={"Accommodation 24hrs (With Food)"}
+            price={400}
+            checkoutId="accomodation_24hrs"
+            description="Upto 24 hrs Stay. Food included"
+          />
+          <AccommodationCard
+            title={"Accommodation 48hrs (With Food)"}
             price={800}
             checkoutId="accomodation_48hrs"
-            description="Upto 48 hrs Stay"
+            description="Upto 48 hrs Stay. Food included"
           />
         </div>
       </div>
