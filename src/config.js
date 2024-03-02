c5c68e68b0d0075cc6cbe9a10533825db03a64e0
Pragadesh BS <pragadeshbs@protonmail.com>
2024-03-02 17:15:02 +0530

    Update API URL in config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
-  apiUrl: "https://samhita24-backend.vercel.app",
+  // apiUrl: "https://samhita24-backend.vercel.app",
   // apiUrl: "http://192.168.0.100:5000",
-  // apiUrl: "samhita-api.azurewebsites.net",
+  apiUrl: "https://samhita-api.azurewebsites.net",
 };
 
 export default config;
