0e4510972931530ed0214ff70d286a45e1f198bb
Pragadesh BS <pragadeshbs@protonmail.com>
2024-03-08 07:13:44 +0530

    Add new prize money information to MyMarquee component

--- src/components/navbar/MyMarquee.jsx
+++ src/components/navbar/MyMarquee.jsx
@@ -4,6 +4,7 @@ const MyMarquee = () => (
   <Marquee>
     <div className="flex text-xl my-3">
       <div className="mx-10">25+ Intern offers</div>
+      <div className="mx-10">2L+ Prize Money</div>
       <div className="flex gap-2 mx-10">
         <div>
           <img src="/logos/decathlon.png" alt="decathlon" className="h-7" />
