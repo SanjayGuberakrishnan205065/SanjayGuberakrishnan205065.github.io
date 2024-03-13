6bc5a842612afe29a2ee636ed2883afb75ea2c2e
Pragadesh BS <pragadeshbs@protonmail.com>
2024-03-13 17:52:24 +0530

    Refactor AccommodationTimings component to display participants by category

--- src/pages/all-stats/accommodation-timings/AccommodationTimings.jsx
+++ src/pages/all-stats/accommodation-timings/AccommodationTimings.jsx
@@ -12,13 +12,15 @@ const AccommodationTimings = () => {
   const { token } = useAuthContext();
   const [loading, setLoading] = useState(true);
   const [accommodationTimings, setAccommodationTimings] = useState([]);
+  const [
+    accommodationCategoryParticipantsMap,
+    setAccommodationCategoryParticipantsMap,
+  ] = useState({});
   const columns = [
     { field: "checkIn", headerName: "Check in", width: 170 },
     { field: "checkOut", headerName: "Check out", width: 170 },
     { field: "name", headerName: "Name", width: 100 },
     { field: "gender", headerName: "Gender", width: 130 },
-    { field: "tickets", headerName: "Purchased Tickets", width: 150 },
-    { field: "count", headerName: "Tickets Count", width: 100 },
     {
       field: "mobile",
       headerName: "Mobile",
@@ -32,37 +34,50 @@ const AccommodationTimings = () => {
   useEffect(() => {
     const fetchAccommodationTimings = () => {
       axios
-        .get("/api/accommodation-timings/all", {
+        .get("/api/tickets/accommodation-participants", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         })
         .then((res) => {
-          console.log(res.data.accommodationTimings);
-          const accommodationTimings = res.data.accommodationTimings;
-          setAccommodationTimings(
-            accommodationTimings.map((accommodationTiming) => {
-              accommodationTiming.id = accommodationTiming._id;
-              accommodationTiming.name = accommodationTiming.user.userName;
-              accommodationTiming.tickets =
-                accommodationTiming.verifiedAccommodationTickets.join(", ");
-              accommodationTiming.count =
-                accommodationTiming.verifiedAccommodationTickets.length;
-              accommodationTiming.mobile = accommodationTiming.user.mobile;
-              accommodationTiming.email = accommodationTiming.user.email;
-              accommodationTiming.department = accommodationTiming.user.dept;
-              accommodationTiming.college = accommodationTiming.user.college;
-              accommodationTiming.gender = accommodationTiming.user.gender;
-              accommodationTiming.checkIn = formatDateTimeWithTimezone(
-                accommodationTiming.checkIn.slice(0, 16)
-              );
-              accommodationTiming.checkOut = formatDateTimeWithTimezone(
-                accommodationTiming.checkOut.slice(0, 16)
-              );
-              return accommodationTiming;
-            })
+          const data = res.data.checkoutOutIdParticipantsMap;
+          const accommodationCategories = Object.keys(data);
+          const tempAccommodationCategoryParticipantsMap = {};
+
+          accommodationCategories.forEach((category) => {
+            const currentCategory =
+              category === "accommodation_48hrs_no_food_new"
+                ? "accommodation_48hrs_no_food"
+                : category;
+            if (!tempAccommodationCategoryParticipantsMap[currentCategory]) {
+              tempAccommodationCategoryParticipantsMap[currentCategory] = [];
+            }
+            tempAccommodationCategoryParticipantsMap[currentCategory] = data[
+              currentCategory
+            ].map((participant, index) => {
+              participant.id = participant._id + index;
+              participant.name = participant.userName;
+              participant.mobile = participant.mobile;
+              participant.email = participant.email;
+              participant.department = participant.dept;
+              participant.college = participant.college;
+              participant.checkIn = participant?.accommodationTiming?.checkIn
+                ? formatDateTimeWithTimezone(
+                    participant.accommodationTiming.checkIn.slice(0, 16)
+                  )
+                : "Not specified";
+              participant.checkOut = participant?.accommodationTiming?.checkOut
+                ? formatDateTimeWithTimezone(
+                    participant.accommodationTiming.checkOut.slice(0, 16)
+                  )
+                : "Not specified";
+              return participant;
+            });
+          });
+
+          setAccommodationCategoryParticipantsMap(
+            tempAccommodationCategoryParticipantsMap
           );
-          setAccommodationTimings(res.data.accommodationTimings);
           setLoading(false);
         })
         .catch((err) => {
@@ -82,13 +97,27 @@ const AccommodationTimings = () => {
   return (
     <div>
       <Typography variant="h3">Accommodation Timings</Typography>
-      <ExportToExcel
-        data={accommodationTimings}
-        fileName="Accommodation Timings"
-      />
-      <div className="my-3">
-        <DataTable rows={accommodationTimings} columns={columns} />
-      </div>
+      {Object.keys(accommodationCategoryParticipantsMap).map(
+        (category, index) => {
+          return (
+            <div key={index} className="my-10">
+              <div className="flex justify-between items-center">
+                <Typography variant="h5" className="my-5">
+                  {category.replaceAll("_", " ")}
+                </Typography>
+                <ExportToExcel
+                  data={accommodationCategoryParticipantsMap[category]}
+                  fileName="Accommodation Timings"
+                />
+              </div>
+              <DataTable
+                rows={accommodationCategoryParticipantsMap[category]}
+                columns={columns}
+              />
+            </div>
+          );
+        }
+      )}
     </div>
   );
 };
